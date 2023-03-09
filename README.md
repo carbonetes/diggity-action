ec8cdc88a78d0994de00cf0fc2ce36b76a89427a
Carbonetes <eng@carbonetes.com>
2023-03-09 16:26:50 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+<p align="center">
+<img src="assets/diggity-black.png" style="display: block; margin-left: auto; margin-right: auto; width: 50%;">
+</p>
+
+# Diggity Github Action
+A Github Action that utilizes [Diggity](https://github.com/carbonetes/diggity#readme) to generate software bill-of-materials (SBOM).
+
+## Directory Scanning
+
+```yaml
+name: Diggity Action
+on: [push, pull_request]
+jobs:
+  diggity:
+    runs-on: ${{matrix.os}}
+    strategy:
+      matrix:
+        os: [ubuntu-latest] # can add more os: windows-latest, macOS-latest
+    steps:
+      - name: Run carbonetes/diggity # runs the github action of diggity
+        uses: carbonetes/diggity@v1.0.0 # runs the github action using this version
+        with: # user’s input reference for scanning options, results that diggity-action supports.
+          directory: "." # path to the directory to be scanned
+          output_type: json # desired SBOM output type (default table)
+          enabled_parsers: apk,go # selected parsers enabled (default all)
+
+```
+
+## License
+
+[Apache 2.0](https://choosealicense.com/licenses/apache-2.0/)
---
+++ index.js
@@ -0,0 +1,112 @@
+/** import */
+const https = require('https');
+const fs = require('fs');
+// npm
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+/**  */
+
+/** var */
+var directoryInput;
+var scanOption;
+
+/** const */
+const DIRECTORY = 'directory';
+const TABLE = 'table';
+const ALL = 'all'
+
+
+async function run() {
+    try {
+        // Download the script using https
+        const options = {
+            hostname: 'raw.githubusercontent.com',
+            path: '/carbonetes/diggity/main/install.sh',
+            method: 'GET'
+        };
+        const request = https.request(options, response => {
+            let script = '';
+            response.on('data', chunk => {
+                script += chunk;
+            });
+            response.on('end', async () => {
+                // Save the script to a file
+                await fs.promises.writeFile('./install.sh', script);
+
+                // Make the script executable
+                await exec.exec('chmod', ['+x', './install.sh']);
+
+                // Run the script with the -d option to specify the installation directory
+                await exec.exec('./install.sh');
+
+                // Installation successful
+                core.info('Diggity has been installed');
+
+
+                // Check scan option based on user's input
+                scanOption = checkScanOption();
+
+                // Call the diggity binary
+                await constructCommandExec(scanOption)
+            });
+        });
+        request.on('error', error => {
+            core.setFailed(error.message);
+        });
+        request.end();
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+// Check user's input and set scan option
+function checkScanOption() {
+    directoryInput = core.getInput('directory', { required: true })
+    if (directoryInput !== null || directoryInput !== '') {
+        return DIRECTORY;
+    }
+}
+
+// Check user's input for output type
+function checkOutputType() {
+    let outputType = core.getInput('output_type')
+    if (outputType === null || outputType === '') {
+        return TABLE;
+    }
+    return outputType
+}
+
+// Check user's input for enabled parsers
+function checkEnabledParsers() {
+    let enabledParsers = core.getInput('enabled_parsers')
+    if (enabledParsers === null || enabledParsers === '' || enabledParsers.toUpperCase() === ALL) {
+        return ALL;
+    }
+    return enabledParsers
+}
+
+async function constructCommandExec(scanOption) {
+    // Check scan option
+    switch (scanOption) {
+        case DIRECTORY:
+            // Check for output type
+            const outputType = checkOutputType()
+            let args = ["-d", directoryInput, "-o", outputType]
+
+            // Check for enabled parsers
+            const enabledParsers = checkEnabledParsers()
+            if (enabledParsers !== ALL) args.push(`--enabled-parsers=${enabledParsers}`)
+
+            // Execute Diggity
+            exec.exec('./bin/diggity', args);
+            break;
+
+        default:
+            core.setFailed('Scan Option not found')
+            break;
+    }
+}
+
+// Start diggity-Action
+run();
